d6dd0f43e2f2312fea51c238cb5ae305428343d8
Sakshi Aggarwal <60293845+sakshi0502@users.noreply.github.com>
2020-06-23 10:23:16 +0530

    Update polymorphism-II.java

--- Inheritance/polymorphism-II.java
+++ Inheritance/polymorphism-II.java
@@ -1,5 +1,5 @@
 //Consider the trunk calls of a telephone exchange. A trunk call can be ordinary, urgent or lightning. The charges depend on the duration and the type of the call. Write a 
-program using the concept of polymorphism in Java to calculate the charges.
+//program using the concept of polymorphism in Java to calculate the charges.
 
 class TrunkCalls
 {
